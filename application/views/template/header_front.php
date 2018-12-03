74377f6b9c6c4f513bfd75c79577c0a3f44615dc
Suvi Sanusi <suvi.7888@gmail.com>
2018-12-03 14:03:05 +0700

    ganti link

--- application/views/template/header_front.php
+++ application/views/template/header_front.php
@@ -36,7 +36,7 @@
       <nav class="navbar navbar-static-top">
         <div class="container">
           <div class="navbar-header">
-            <a href="<?php echo base_url('assets/index2.html');?>" class="navbar-brand"><b>SIMOLEG KESDM</b></a>
+            <a href="<?php echo base_url();?>" class="navbar-brand"><b>SIMOLEG KESDM</b></a>
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
               <i class="fa fa-bars"></i>
             </button>
